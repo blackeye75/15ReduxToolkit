8c68d6b2b042df50e2b0f46b7f613839c81b6ef3
blackeye75 <priyanshuraj22275@gmail.com>
2024-06-06 00:54:33 +0530

    redux setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl underline' >Hello World</h1>
+    </>
+  )
+}
+
+export default App
