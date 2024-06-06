01309953cbd934648e6930f86d9c1aecee54acef
blackeye75 <priyanshuraj22275@gmail.com>
2024-06-06 23:51:12 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+
 import './App.css'
+import AddTodo from './component/AddTodo'
+import Todos from './component/Todos'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl underline' >Hello World</h1>
+      <h1 className='text-2xl text-center' >Your Todo's</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
